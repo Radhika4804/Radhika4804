96d73ba04ac12537d678543ae31ff69fef07df3d
Radhika4804 <radhika48044@gmail.com>
2024-10-15 23:30:51 +0530

    file.c

--- assignment question 2.c
+++ assignment question 2.c
@@ -1,17 +1,2 @@
 
-					accounts[accountNumber - 1] -= amount;
-					printf("Withdrew %.2f from account %d\n", amount, accountNumber);
-				} else {
-					printf("Insufficient balance in account %d\n", accountNumber);
-				}
-				break;
-			case 3:
-				printf("Balance of account %d: %.2f\n", accountNumber, accounts[accountNumber - 1]);
-				break;
-			default:
-				printf("Invalid choice!\n");
-				
-		}
-	} 	
-				return 0;
-}
+					
